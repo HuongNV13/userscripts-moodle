92d4a7018c8bf3006edd25ed7a4c340fd7cf4a68
Andrew Nicols <andrew@nicols.co.uk>
2024-05-28 08:47:46 +0800

    4.4 release

--- pull-request-helper.user.js
+++ pull-request-helper.user.js
@@ -7,7 +7,7 @@
 // @homepage      http://github.com/andrewnicols/userscripts-moodle
 // @namespace     http://userscripts.andrewrn.co.uk
 // @downloadURL   https://github.com/andrewnicols/userscripts-moodle/raw/fork/pull-request-helper.user.js
-// @version       4.03.0.1
+// @version       4.04.0.0
 // ==/UserScript==
 
 var userScript = function() {
@@ -17,6 +17,11 @@ var userScript = function() {
                 shortname: 'main',
                 customField: '10111',
                 branchname: 'main'
+            },
+            {
+                shortname: '404',
+                customField: '16910',
+                branchname: 'MOODLE_404_STABLE'
             },
             {
                 shortname: '403',
